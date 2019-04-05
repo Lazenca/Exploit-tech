c150cac251e4beee84792ebcfd54050483a7d0de
lazenca.0x0 <lazenca.0x0@gmail.com>
2019-04-05 23:18:31 +0900

    Upload files related to the "03.RTL(Return to libc)" page

---
+++ 03.RTL(Return to libc)/02.RTL(Return to Libc) - x64/ret2libc.c
@@ -0,0 +1,15 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <dlfcn.h>
+ 
+void vuln(){
+    char buf[50] = "";
+    void (*printf_addr)() = dlsym(RTLD_NEXT, "printf");
+    printf("Printf() address : %p\n",printf_addr);
+    read(0, buf, 100);
+}
+ 
+void main(){
+    vuln();
+}
---
+++ 03.RTL(Return to libc)/02.RTL(Return to Libc) - x64/test.c
@@ -0,0 +1,11 @@
+//gcc -o test test.c
+#include <stdlib.h>
+#include <stdio.h>
+ 
+void vuln(int a,int b,int c,int d){
+        printf("%d, %d, %d, %d",a,b,c,d);
+}
+ 
+void main(){
+        vuln(1,2,3,4);
+}
